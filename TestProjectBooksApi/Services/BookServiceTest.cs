04df79c8b1b590d9ff92347a2e66e0b2a12d5366
cloudsystems22 <cloudsystems22@gmail.com>
2021-06-25 17:42:46 -0300

    Implementations unit test

---
+++ TestProjectBooksApi/Services/BookServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using BooksApi;
+using BooksApi.Services;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace TestProjectBooksApi.Services
+{
+    public class BookServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Get()
+        {
+            var bookService = new Mock<BookService>();
+            bookService.Setup(s => s.Get()).Verifiable();
+
+            Assert.Pass("");
+        }
+    }
+}
